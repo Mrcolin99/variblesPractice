162c9eca8d4dca09f04a22fa48bff6a84aa47339
Colin Ciervo <colinciervo@@gmail.com>
2022-06-07 18:13:25 -0400

    I cant spell

--- varibles.js
+++ varibles.js
@@ -6,9 +6,11 @@ var daysWithoutSleep = 0
 var hungry = true
 var tired = true
 var discourages = false
+var foolish = "IVEBEENSPELLINGVARIABLESWRONGTHISWHOLETIMEOHMYGOD"
 
 name = "colin"
 coolness = 2
 console.log(daysWithoutSleep)
 console.log(coolness)
 console.log(name)
+console.log(foolish)
